a760f165c72d07c2305644e3b8901c4dd2852a62
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-23 14:28:35 +0400

    developed

--- TODO-APP/src/components/Todo.jsx
+++ TODO-APP/src/components/Todo.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { IoIosRemoveCircle } from "react-icons/io";
+import { FaEdit } from "react-icons/fa";
 
 function Todo() {
     return (
         <div>
-
+            <div>
+                I am first TODO
+            </div>
+            <div>
+                <IoIosRemoveCircle />
+                <FaEdit />
+            </div>
         </div>
     )
 }
