8aca02d2c91785339497c0e04fd2990ed2ea7f5f
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-23 14:00:19 +0400

    developed

---
+++ TODO-APP/src/components/TodoCreate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TodoCreate() {
+    return (
+        <div className='todo-create'>
+            <input type="text" placeholder='Enter TODO' />
+            <button>Enter TODO</button>
+        </div>
+    )
+}
+
+export default TodoCreate
