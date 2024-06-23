6ac3c4466ad726faf7ebbc5f758a7f1865eda57b
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-23 14:04:29 +0400

    developed

--- TODO-APP/src/App.jsx
+++ TODO-APP/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import TodoCreate from './components/TodoCreate'
 
 function App() {
 
   return (
     <div>
-
+      <TodoCreate />
     </div>
   )
 }
